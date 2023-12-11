ae8f915daf7c4320ff6537e2bc8b62b657b53427
Arman Vardanyan <vardanyanarman148@yahoo.com>
2023-12-11 21:20:15 +0100

    done main page and navigation
    this commit have main assets for project, navigation, most pages and huge part of main page

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSingUp from './Pages/LoginSingUp';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/men' element={<ShopCategory category="men" />}/>
+          <Route path='/womens' element={<ShopCategory category="women" />}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSingUp/>}/>
+        </Routes>
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
